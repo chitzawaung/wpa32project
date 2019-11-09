86472053c99b1261cde35f3157395c116cd8e115
cza <chitzawaung31993@gmail.com>
2019-11-10 02:21:44 +0630

    route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Author;
+use App\Tag;
+use App\Blog;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +17,28 @@
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("blog_query", function() {
+    $blog = Blog::find(3);
+    var_dump($blog->author->name);
+    $tags = $blog->tags;
+    foreach($tags as $tag) {
+        var_dump($tag->name);
+    }
+    return "Yay!";
+});
+
+Route::get("blog_entry" , function() {
+    $faker = Faker\Factory::create();
+    $author = Author::find(2);
+    $tags = Tag::where("id",'<', 4)->get();
+    $blog = Blog::create([
+        'blog_title' => $faker->sentence,
+        'blog_body' => $faker->paragraph,
+        'author_id' => $author->id
+    ]);
+    $blog->tags()->attach($tags);
+    $blog->save();
+
 });
