39acef595bc67634e84e1f2ee168d4b418929a67
cza <chitzawaung31993@gmail.com>
2019-11-10 02:18:57 +0630

    seeder

---
+++ database/seeds/BlogTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Author;
+use App\Tag;
+use App\Blog;
+
+class BlogTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // $this->call(AuthorTableSeeder::class);
+        // $this->call(TagTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
